7387fd3139554865eb049bfda3b74b6f285761f9
week <weekdev1@gmail.com>
2025-06-26 23:47:35 +0900

    community add

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 
-export default [index("common/pages/home-page.tsx")] satisfies RouteConfig;
+export default [
+    index("common/pages/home-page.tsx"),
+    ...prefix("/community", [
+        index("features/community/pages/community-page.tsx"),
+      ]),
+] satisfies RouteConfig;
